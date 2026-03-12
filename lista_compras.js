c243f80cfa2f13fdddcdc2a039f28c6bf1e83e13
Henrique Costa <henriqueduartemartinscmd@gmail.com>
2026-03-12 12:11:46 -0300

    atividade_04

---
+++ lista_compras.js
@@ -0,0 +1,9 @@
+let lista_compras = ["Arroz", "Feijão", "Carne", "Farinha"];
+lista_compras.forEach(item =>{
+console.log(item);
+});
+
+let cesta_frutas = ["Tomate","Limão","Uva"]
+cesta_frutas.forEach(item=>{
+console.log(item);
+});
